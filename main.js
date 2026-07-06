6ed616a8eb00ba58825d8beb2746fc898fdbd9ca
Gabryel Tomaz Fernandes <gabryeltomazfernandes@ufu.br>
2026-07-06 10:15:27 -0300

    [fix] modal-agendamento abrindo e fechando corretamente agora

--- main.js
+++ main.js
@@ -2,27 +2,30 @@ const container = document.getElementById("container-modal");
 
 import { inicializarLogicaModal } from "./agendamento page/agendamento.js";
 
-document
-.getElementById("abrir-agendamento")
-.addEventListener("click", () => {
+function abrirModal() {
 
-    if(container.innerHTML.trim() !== ""){
+    const modal = document.getElementById("modal-agendamento");
 
-        document
-        .getElementById("modal-agendamento")
-
+    if (modal) {
+        modal.classList.remove("hidden");
+        document.body.classList.add("overflow-hidden");
         return;
     }
 
     fetch("../agendamento page/agendamento.html")
         .then(response => response.text())
         .then(html => {
+
             container.innerHTML = html;
 
             inicializarLogicaModal();
-        });
 
-});
+        })
+        .catch(error => console.error(error));
+}
 
-//=======================================================================================
-
+document
+    .querySelectorAll(".abrir-agendamento")
+    .forEach(botao => {
+        botao.addEventListener("click", abrirModal);
+    });
